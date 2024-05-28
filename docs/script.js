740ba74dd63d4f3491852a91184d76a34b0460fa
Janek21 <116804456+Janek21@users.noreply.github.com>
2024-05-29 00:00:17 +0200

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -15,7 +15,7 @@ document.getElementById("butt2").onclick = function() {
 document.getElementById("butt3").onclick = function() {
 	//document.getElementById("heading").remove();
 	document.body.style.background="yellow";
-	document.getElementById("heading").style.color = "green";
+	document.getElementById("heading").style.display = "none";
 }
 
 document.getElementById("resetter").onclick = function() {
@@ -23,5 +23,6 @@ document.getElementById("resetter").onclick = function() {
 	document.getElementById('demo').innerHTML="These are buttons, I'll eventually use them for something";
 	document.getElementById('demo').style.fontSize='14pt';
 	document.body.style.background="#21421e";
+	document.getElementById("heading").style.display = "block";
 	document.getElementById("heading").style.color = "#e69500"
 }
